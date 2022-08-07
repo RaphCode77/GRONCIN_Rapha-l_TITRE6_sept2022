abbdc681ea0375ce9971752ffaa00ad98142981f
Raphaël Groncin <raphael.groncin>
2022-08-07 15:48:32 +0200

    ajout fonction publique add dans ArticlesController.php

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,15 +4,33 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Flash'); // Charge le FlashComponent
+    }
     public function index()
     {
-        $articles = $this->Articles->find()->all();
-        $this->set(compact('articles'));
+        $this->set('articles', $this->Articles->find()->all());
     }
 
     public function view($id = null)
     {
         $article = $this->Articles->get($id);
         $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Votre article a été sauvegardé.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Impossible d\'ajouter votre article.'));
+        }
+        $this->set(compact('article', $article));
     }
 }
