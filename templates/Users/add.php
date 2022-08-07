3bc346be4b65ea282acd17155a542459898d243a
Raphaël Groncin <raphael.groncin>
2022-08-07 17:08:52 +0200

    création dossier gestion des utilisateurs dans le dossier templates

---
+++ templates/Users/add.php
@@ -0,0 +1,15 @@
+<!-- templates/Users/add.php -->
+
+<div class="users form">
+<?= $this->Form->create($user) ?>
+    <fieldset>
+        <legend><?= __('Ajouter un utilisateur') ?></legend>
+        <?= $this->Form->control('email') ?>
+        <?= $this->Form->control('password') ?>
+        <?= $this->Form->control('role', [
+            'options' => ['admin' => 'Admin', 'author' => 'Author']
+        ]) ?>
+    </fieldset>
+<?= $this->Form->button(__('Ajouter')); ?>
+<?= $this->Form->end() ?>
+</div>
